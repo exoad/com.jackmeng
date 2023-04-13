5510c5ed95494f1ee80548b66419019612372460
Jack M <jackmeng0814@gmail.com>
2023-04-13 17:59:24 -0400

    Update stl.java

--- src/main/java/com/jackmeng/stl/stl.java
+++ src/main/java/com/jackmeng/stl/stl.java
@@ -1,13 +1,14 @@
 package com.jackmeng.stl;
 
-public enum stl
+public final class stl
 {
-    STL_VERSION("1.1"),
-    STL_TYPE("open_beta");
-
-    final String name;
-    stl(String str)
-    {
-        this.name = str;
-    }
+    private stl() {}
+    
+    public static final String STL_VERSION = "0.0.1";
+    public static final String STL_LATEST_RELEASE = "0.0.1";
+    public static final long STL_BASE = 2023_4_13L;
+    
+    public static final String[] TARGETS = new String[] { "win32", "linux", "osx" };
+    public static final short[] TARGET_ARCHS = { 32, 64 };
+    
 }
